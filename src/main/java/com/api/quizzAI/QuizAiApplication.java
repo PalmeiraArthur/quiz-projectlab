291f61b19bce876297b88191188e1b4c765b2ac6
gpossas <possasg@gmail.com>
2025-08-13 16:29:21 -0300

    first commit!

---
+++ src/main/java/com/api/quizzAI/QuizAiApplication.java
@@ -0,0 +1,13 @@
+package com.api.quizzAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/quizzAI/QuizAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.quizzAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
