1d98ad6dd18d3e8c4b75cb322b32eae3a607e56a
gpossas <possasg@gmail.com>
2025-10-07 19:45:22 -0300

    refact: move corf config to infra directory

--- src/main/java/com/api/quizAI/infra/config/CorsConfig.java
+++ src/main/java/com/api/quizAI/infra/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.api.quizAI.config;
+package com.api.quizAI.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
